683ede85a4cc71c7115d2da2f83f02845f9889d8
Shireee <kirog11az@gmail.com>
2023-10-25 19:53:34 +0300

    Add autolvl generation

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    const int maxLevel = 30;
+    [Range(1, maxLevel)]
+    public int level = 1;
+    public float ballVelocityMult = 0.02f;
+    public GameObject bluePrefab;
+    public GameObject redPrefab;
+    public GameObject greenPrefab;
+    public GameObject yellowPrefab;
+    public GameObject ballPrefab;
+
     void Start()
     {
         Cursor.visible = false;
+        StartLevel();
     }
-
-    // Update is called once per frame
+    void CreateBlocks(GameObject prefab, float xMax, float yMax,
+     int count, int maxCount)
+    {
+        if (count > maxCount)
+            count = maxCount;
+        for (int i = 0; i < count; i++)
+            Instantiate(prefab,
+            new Vector3((Random.value * 2 - 1) * xMax,
+            Random.value * yMax, 0),
+            Quaternion.identity);
+    }
+    void CreateBalls()
+    {
+        int count = 2;
+        for (int i = 0; i < count; i++)
+        {
+            var obj = Instantiate(ballPrefab);
+            var ball = obj.GetComponent<BallScript>();
+            ball.ballInitialForce += new Vector2(10 * i, 0);
+            ball.ballInitialForce *= 1 + level * ballVelocityMult;
+        }
+    }
+    void StartLevel()
+    {
+        var yMax = Camera.main.orthographicSize * 0.8f;
+        var xMax = Camera.main.orthographicSize * Camera.main.aspect * 0.85f;
+        CreateBlocks(bluePrefab, xMax, yMax, level, 8);
+        CreateBlocks(redPrefab, xMax, yMax, 1 + level, 10);
+        CreateBlocks(greenPrefab, xMax, yMax, 1 + level, 12);
+        CreateBlocks(yellowPrefab, xMax, yMax, 2 + level, 15);
+        CreateBalls();
+    }
     void Update()
     {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
