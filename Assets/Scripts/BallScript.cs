0089fb57eb1d522c3761166e922a0aa2b516184b
Shireee <kirog11az@gmail.com>
2023-10-25 22:20:09 +0300

    Add interraction with J - change ball direction

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -13,33 +13,49 @@ public class BallScript : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        playerObj = GameObject.FindGameObjectWithTag("Player");
+        playerObj = GameObject.FindGameObjectWithTag("Player");// init player object
         deltaX = transform.position.x;
-        audioSrc = Camera.main.GetComponent<AudioSource>();
+        audioSrc = Camera.main.GetComponent<AudioSource>(); // init audio source
     }
 
     void Update()
     {
         if (rb.isKinematic)
+        {
             if (Input.GetButtonDown("Fire1"))
             {
-                rb.isKinematic = false;
-                rb.AddForce(ballInitialForce);
+                rb.isKinematic = false; // switching to Dynamic body type
+                rb.AddForce(ballInitialForce); // giving initial force to the ball
             }
-            else
+            else // if not kinematic follow to the player object
             {
                 var pos = transform.position;
                 pos.x = playerObj.transform.position.x + deltaX;
                 transform.position = pos;
             }
+        }
+
+        // Changing ball trajectory by pressing J
+        if (!rb.isKinematic && Input.GetKeyDown(KeyCode.J))
+        {
+            var v = rb.velocity;
+            if (Random.Range(0, 2) == 0)
+                v.Set(v.x - 0.1f, v.y + 0.1f);
+            else
+                v.Set(v.x + 0.1f, v.y - 0.1f);
+            rb.velocity = v;
+        }
     }
 
+    // Add get-score sound when destroying block
     void OnTriggerEnter2D(Collider2D other)
     {
         if (gameData.sound) audioSrc.PlayOneShot(loseSound, 5);
         Destroy(gameObject);
         playerObj.GetComponent<PlayerScript>().BallDestroyed();
     }
+
+    // Add hit sound when touch block
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (gameData.sound)  audioSrc.PlayOneShot(hitSound, 4);
